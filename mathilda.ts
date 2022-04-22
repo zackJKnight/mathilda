92b76ebde7f9c56c00a30d7cd6f208018178918c
Sam Knight <robogamingbysam@icloud.com>
2022-04-21 19:00:23 -0500

    Add scaffold for generic search

--- mathilda.ts
+++ mathilda.ts
@@ -244,6 +244,10 @@ router.get("/generic/product", async (ctx) => {
   }
 })
 
+router.get("/generic/search", async (ctx) => {
+  ctx.response.redirect(`https://wishlily.app/amazon/search?q=${ctx.request.url.searchParams.get('q')}`)
+})
+
 router.get('/embed', async (ctx) => {
   try {
     const userId = ctx.request.url.searchParams.get('userId')
